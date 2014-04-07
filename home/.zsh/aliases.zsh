d323a8bab30b18853dcbb7856ea5b6499d686c52
Matthew M. Keeler <matthew.keeler@researchsquare.com>
2014-04-07 06:52:04 -0400

    zsh: alias to change to git root

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias homesick="~/.homeshick"
 alias tmux='tmux -2 -u'
 alias ec='emacsclient -c'
+alias gr='[ ! -z `git rev-parse --show-toplevel` ] && cd `git rev-parse --show-toplevel || pwd`'
 
 [ -e ~/.aliases.local ] && . ~/.aliases.local || true
