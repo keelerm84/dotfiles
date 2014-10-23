8e74a91b67f1498a966bd30ef163c036a21a8133
Matthew M. Keeler <matthew.keeler@researchsquare.com>
2014-10-23 16:26:41 -0400

    zsh: Various shortcuts for git

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -1,6 +1,15 @@
 alias homesick="~/.homeshick"
 alias tmux='tmux -2 -u'
 alias ec='emacsclient -c'
+
+
 alias gr='[ ! -z `git rev-parse --show-toplevel` ] && cd `git rev-parse --show-toplevel || pwd`'
+alias gdc='git diff --cached'
+alias gd='git diff'
+alias gap='git add -p'
+alias ga='git add'
+alias ci='git commit'
+alias cia='git commit --amend'
+alias gs='git status'
 
 [ -e ~/.aliases.local ] && . ~/.aliases.local || true
