71b1d679d2f8c1ce857805e0370cdacc69f8e1ae
Matthew M. Keeler <keelerm84@gmail.com>
2014-11-05 22:35:25 -0600

    zsh: Color man pages

--- home/.zsh/functions.zsh
+++ home/.zsh/functions.zsh
@@ -1,5 +1,17 @@
 # Convenient function to allow local overrides for all sourced files
 function load_file() {
-  [ -e ~/.zsh/${1} ] && . ~/.zsh/${1} || true
-  [ -e ~/.zsh.local/${1} ] && . ~/.zsh.local/${1} || true
+    [ -e ~/.zsh/${1} ] && . ~/.zsh/${1} || true
+    [ -e ~/.zsh.local/${1} ] && . ~/.zsh.local/${1} || true
+}
+
+man() {
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+        man "$@"
 }
