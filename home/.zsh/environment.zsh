479bbe639a069027a1976e202e33812573571ddd
Matthew M. Keeler <keelerm84@gmail.com>
2015-01-03 21:26:57 -0500

    zsh: Setup default golang variables

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -3,3 +3,6 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 source ~/.xinitrc 2> /dev/null
+
+[ -e "$HOME/Projects/golang" ] && export GOPATH="$HOME/Projects/golang"
+[ -e "/usr/local/go" ] && export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
