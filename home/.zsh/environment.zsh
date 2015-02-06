332ee90b3fd9098ffcb5b22855c07af05e0e2ad4
Matthew M. Keeler <keelerm84@gmail.com>
2015-02-05 23:02:23 -0500

    Ask cask to path if it exists

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -7,3 +7,4 @@ source ~/.xinitrc 2> /dev/null
 
 [ -e "$HOME/Projects/golang" ] && export GOPATH="$HOME/Projects/golang"
 [ -e "/usr/local/go" ] && export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
+[ -e "$HOME/.cask/bin/" ] && export PATH="$PATH:$HOME/.cask/bin/"
