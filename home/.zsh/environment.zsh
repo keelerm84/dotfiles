baf988f732528ae76ad471ca69e5186627d4d7b8
Matthew M. Keeler <keelerm84@gmail.com>
2016-12-13 18:50:37 -0500

    zsh: Setup dircolors

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -3,7 +3,7 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export TERM=xterm-256color
 
-source ~/.xinitrc 2> /dev/null
+eval "$(dircolors ~/.dircolors)"
 
 [ -e "$HOME/Projects/golang" ] && export GOPATH="$HOME/Projects/golang"
 [ -e "/usr/local/go" ] && export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
