49d1e1196f8273880cae86b36d41b72f5670c4ec
Matthew M. Keeler <keelerm84@gmail.com>
2017-03-10 23:11:15 -0500

    Adding missing eval

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -5,7 +5,7 @@ export TERM=xterm-256color
 
 export FZF_CTRL_R_OPTS="--inline-info --exact"
 
-command -v dircolors > /dev/null && dircolors ~/.dircolors
+command -v dircolors > /dev/null && eval "$(dircolors ~/.dircolors)"
 
 [ -e "$HOME/Projects/golang" ] && export GOPATH="$HOME/Projects/golang"
 [ -e "/usr/local/go" ] && export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
