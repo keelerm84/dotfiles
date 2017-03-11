07bfd89433b3a0f8a104f4ebf3a301817c62f591
Joseph Hallenbeck <joe@joehallenbeck.com>
2017-03-10 21:07:43 -0700

    matching style for checking in .xinitrc

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -5,9 +5,7 @@ export TERM=xterm-256color
 
 export FZF_CTRL_R_OPTS="--inline-info --exact"
 
-if whence dircolors >/dev/null; then
-    eval "$(dircolors ~/.dircolors)"
-fi
+command -v dircolors > /dev/null && dircolors ~/.dircolors
 
 [ -e "$HOME/Projects/golang" ] && export GOPATH="$HOME/Projects/golang"
 [ -e "/usr/local/go" ] && export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
