7c21ecf456a2890a8eebb78201c90ee3269676bd
Joseph Hallenbeck <joe@joehallenbeck.com>
2017-03-10 21:12:10 -0700

    zsh: Check if dircolors exists
    dircolors doesn't exist on OSX so we need to verify that command exists before trying to execute it.

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -5,7 +5,7 @@ export TERM=xterm-256color
 
 export FZF_CTRL_R_OPTS="--inline-info --exact"
 
-eval "$(dircolors ~/.dircolors)"
+command -v dircolors > /dev/null && eval "$(dircolors ~/.dircolors)"
 
 [ -e "$HOME/Projects/golang" ] && export GOPATH="$HOME/Projects/golang"
 [ -e "/usr/local/go" ] && export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
