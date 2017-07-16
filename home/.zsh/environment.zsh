f6c73fb10babc8108204e165b1e81e9836b780b8
Matthew M. Keeler <keelerm84@gmail.com>
2017-07-16 00:04:55 -0400

    zsh: Gruvbox-like fzf coloring

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -1,6 +1,10 @@
 export TERM=${TERM:-xterm-256color}
 export LANG=en_US.UTF-8
 
+export FZF_DEFAULT_OPTS='
+    --color fg:223,bg:-1,hl:65,fg+:142,bg+:-1,hl+:108
+    --color info:108,prompt:109,spinner:108,pointer:168,marker:168
+'
 export FZF_CTRL_R_OPTS="--inline-info --exact"
 
 command -v dircolors > /dev/null && eval "$(dircolors ~/.dircolors)"
