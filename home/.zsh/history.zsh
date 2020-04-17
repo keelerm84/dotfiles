d8aeddad5d7cc8ca9ceca369e4077bc254c8b62d
Matthew M. Keeler <keelerm84@gmail.com>
2020-04-17 07:45:29 -0400

    zsh: Clean up duplicate configuration

--- home/.zsh/history.zsh
+++ home/.zsh/history.zsh
@@ -2,11 +2,11 @@ HISTSIZE=10000
 SAVEHIST=10000
 HISTFILE=$HOME/.zsh_history
 
-setopt append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_dups # ignore duplication command history list
-setopt hist_ignore_space
-setopt hist_verify
-setopt inc_append_history
-setopt share_history # share command history data
+setopt APPEND_HISTORY
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_VERIFY
+unsetopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
