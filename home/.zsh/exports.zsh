c444f604c832969898d6a90f6c2ce0a1872525f4
Matthew M. Keeler <keelerm84@gmail.com>
2021-01-17 01:07:49 -0500

    zsh: Disable aws sam cli telemetry reporting

--- home/.zsh/exports.zsh
+++ home/.zsh/exports.zsh
@@ -6,6 +6,7 @@ if [ -e "$HOME/.nvm" ]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use
 fi
+export SAM_CLI_TELEMETRY=0
 
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && . $HOME/.tmuxinator/scripts/tmuxinator || true
 [[ -f $HOME/.homesick/repos/homeshick/homeshick.sh ]] && source $HOME/.homesick/repos/homeshick/homeshick.sh || true
