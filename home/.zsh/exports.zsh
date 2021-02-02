96e03d88834124667d57504f4d39992ad2adb685
Matthew M. Keeler <keelerm84@gmail.com>
2021-02-02 02:23:57 -0500

    zsh: Use editor as man page viewer

--- home/.zsh/exports.zsh
+++ home/.zsh/exports.zsh
@@ -6,6 +6,7 @@ if [ -e "$HOME/.nvm" ]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use
 fi
+export MANPAGER="$EDITOR +Man!"
 export SAM_CLI_TELEMETRY=0
 
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && . $HOME/.tmuxinator/scripts/tmuxinator || true
