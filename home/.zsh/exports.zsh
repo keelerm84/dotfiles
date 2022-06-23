cb49534af07db4428e2dd6dce36cd5eac3ab2c00
Matthew M. Keeler <keelerm84@gmail.com>
2022-06-23 00:17:06 -0400

    zsh: Set BAT_THEME

--- home/.zsh/exports.zsh
+++ home/.zsh/exports.zsh
@@ -3,6 +3,7 @@ command -v nvim > /dev/null && export MANPAGER="$EDITOR +Man!" || export MANPAGE
 export MYSQL_PS1="mysql  \d  \R:\m:\s  "
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export SAM_CLI_TELEMETRY=0
+export BAT_THEME="Solarized (dark)"
 
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && . $HOME/.tmuxinator/scripts/tmuxinator || true
 [[ -f $HOME/.homesick/repos/homeshick/homeshick.sh ]] && source $HOME/.homesick/repos/homeshick/homeshick.sh || true
