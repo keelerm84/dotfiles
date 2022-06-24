b5eaba815f25837907af2b5bebb2e0506cf159fa
Matthew M. Keeler <keelerm84@gmail.com>
2022-06-23 23:38:13 -0400

    Remove annoying highlight functionality

--- home/.config/nvim/lua/core/autocmds.lua
+++ home/.config/nvim/lua/core/autocmds.lua
@@ -8,15 +8,6 @@
 local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 
--- Highlight on yank
-augroup('YankHighlight', { clear = true })
-autocmd('TextYankPost', {
-  group = 'YankHighlight',
-  callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '1000' })
-  end
-})
-
 -- Settings for filetypes:
 -- Set indentation to 2 spaces
 augroup('setIndent', { clear = true })
