0363b346467323c36abbaf05997d1e931bed2dcd
Matthew M. Keeler <keelerm84@gmail.com>
2022-09-10 14:19:55 -0400

    Add function support

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -9,6 +9,7 @@ Website: https://github.com/brainfucksec/neovim-lua
 
 -- Import Lua modules
 require('packer_init')
+require('core/functions')
 require('core/options')
 require('core/autocmds')
 require('core/keymaps')
---
+++ home/.config/nvim/lua/core/functions.lua
@@ -0,0 +1,22 @@
+vim.cmd[[
+" Stolen from FZF's repo since it isn't an exported function
+function! s:get_git_root()
+  if exists('*fugitive#repo')
+    try
+      return fugitive#repo().tree()
+    catch
+    endtry
+  endif
+  let root = split(system('git rev-parse --show-toplevel'), '\n')[0]
+  return v:shell_error ? '' : root
+endfunction
+
+function! functions#SmartFzfSearching()
+    let root = s:get_git_root()
+    if empty(root)
+        Files
+    else
+        GFiles
+    endif
+endfunction
+]]
