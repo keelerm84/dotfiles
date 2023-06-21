201c3fcf4f4e8b6accf6ec5e915fe18a05bb5774
Matthew Keeler <mkeeler@launchdarkly.com>
2023-06-21 08:43:54 -0400

    nvim: tagbar changes

--- home/.config/nvim/lua/packer_init.lua
+++ home/.config/nvim/lua/packer_init.lua
@@ -173,12 +173,75 @@ return packer.startup(function(use)
 
   -- UI {{{
   use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require("indent_blankline").setup {
+        space_char_blankline = " ",
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = false,
+      }
+    end
+  }
+
+  use {
     'nvim-tree/nvim-tree.lua',
     requires = {
       'nvim-tree/nvim-web-devicons',
     }
   }
-  use 'preservim/tagbar'
+  use {
+    'preservim/tagbar',
+    config = function()
+      vim.cmd[[
+      let g:tagbar_type_haskell = {
+        \ 'ctagsbin'  : 'hasktags',
+        \ 'ctagsargs' : '-x -c -o-',
+        \ 'kinds'     : [
+            \  'm:modules:0:1',
+            \  'd:data: 0:1',
+            \  'd_gadt: data gadt:0:1',
+            \  't:type names:0:1',
+            \  'nt:new types:0:1',
+            \  'c:classes:0:1',
+            \  'cons:constructors:1:1',
+            \  'c_gadt:constructor gadt:1:1',
+            \  'c_a:constructor accessors:1:1',
+            \  'ft:function types:1:1',
+            \  'fi:function implementations:0:1',
+            \  'i:instance:0:1',
+            \  'o:others:0:1'
+        \ ],
+        \ 'sro'        : '.',
+        \ 'kind2scope' : {
+            \ 'm' : 'module',
+            \ 'c' : 'class',
+            \ 'd' : 'data',
+            \ 't' : 'type',
+            \ 'i' : 'instance'
+        \ },
+        \ 'scope2kind' : {
+            \ 'module'   : 'm',
+            \ 'class'    : 'c',
+            \ 'data'     : 'd',
+            \ 'type'     : 't',
+            \ 'instance' : 'i'
+        \ }
+      \ }
+      let g:tagbar_type_ruby = {
+        \ 'kinds' : [
+        \ 'm:modules',
+        \ 'c:classes',
+        \ 'd:describes',
+        \ 'C:contexts',
+        \ 'i:things described with "it"',
+        \ 'f:methods',
+        \ 'F:singleton methods'
+        \ ]
+        \ }
+      ]]
+    end
+  }
   use 'kyazdani42/nvim-web-devicons'
 
   -- Color themes {{{
