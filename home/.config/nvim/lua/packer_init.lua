04e8d708a9afc9b5fc083818c96d21646d203cbc
Matthew M. Keeler <keelerm84@gmail.com>
2025-04-09 22:43:24 -0400

    vim: Simplify indent config

--- home/.config/nvim/lua/packer_init.lua
+++ home/.config/nvim/lua/packer_init.lua
@@ -177,13 +177,8 @@ return packer.startup(function(use)
     config = function()
       require("ibl").setup {
         scope = {
-       enabled = true,
-       show_start = true,
-       show_end = false,
-       injected_languages = false,
-       highlight = { "Function", "Label" },
-       priority = 500,
-          }
+          enabled = true,
+        }
       }
     end
   }
