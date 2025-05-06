f7dc772e02c6eb03eee49f86dddaa9ee63556fff
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-06 19:23:16 -0400

    vim: Cleanup unused autocmds and git commit

--- home/.config/nvim/lua/core/autocmds.lua
+++ home/.config/nvim/lua/core/autocmds.lua
@@ -28,27 +28,6 @@ autocmd('TermOpen', {
   command = 'setlocal listchars= nonumber norelativenumber nocursorline',
 })
 
-autocmd('TermOpen, TermEnter', {
-  pattern = 'term://*',
-  command = 'startinsert'
-})
-
--- Add blank line and move into insert mode when committing via Neogit
-autocmd('BufEnter', {
-  pattern = 'COMMIT_EDITMSG',
-  callback = function()
-    local ok, wasEntered = pcall(vim.api.nvim_buf_get_var, 0, 'mmk:wasEntered')
-
-    if ok and wasEntered then
-      return
-    end
-
-    vim.api.nvim_buf_set_var(0, 'mmk:wasEntered', 1)
-    vim.api.nvim_command('normal O')
-    vim.cmd('startinsert')
-  end
-})
-
 -- Close terminal buffer on process exit
 autocmd('BufLeave', {
   pattern = 'term://*',
