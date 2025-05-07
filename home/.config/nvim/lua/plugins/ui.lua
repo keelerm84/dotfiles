e056f48706ba55c67c45809e901f62689307947a
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-06 23:03:48 -0400

    vim: Add basic UI plugins

---
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,79 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    init = function()
+      vim.ggruvbox_material_foreground = 'material'
+      vim.ggruvbox_material_background = 'soft'
+      vim.ggruvbox_material_disable_italic_comment = 1
+    end,
+    config = function()
+      vim.cmd.colorscheme "gruvbox-material"
+    end
+  },
+
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
+    opts = {
+      scope = { enabled = true, }
+    }
+  },
+
+  {
+    'preservim/tagbar',
+    keys = {
+      { '<leader>tt', ':TagbarOpen fj<CR>', silent = true },
+      { '<leader>tc', ':TagbarClose<CR>', silent = true },
+    },
+    init = function()
+      vim.g.tagbar_type_haskell = {
+        ctagsbin = 'hasktags',
+        ctagsargs = '-x -c -o-',
+        kinds = {
+          'm:modules:0:1',
+          'd:data: 0:1',
+          'd_gadt: data gadt:0:1',
+          't:type names:0:1',
+          'nt:new types:0:1',
+          'c:classes:0:1',
+          'cons:constructors:1:1',
+          'c_gadt:constructor gadt:1:1',
+          'c_a:constructor accessors:1:1',
+          'ft:function types:1:1',
+          'fi:function implementations:0:1',
+          'i:instance:0:1',
+          'o:others:0:1',
+        },
+        sro = '.',
+        kind2scope = {
+          m = 'module',
+          c = 'class',
+          d = 'data',
+          t = 'type',
+          i = 'instance',
+        },
+        scope2kind = {
+          module = 'm',
+          class = 'c',
+          data = 'd',
+          type = 't',
+          instance = 'i',
+        }
+      }
+
+      vim.g.tagbar_type_ruby = {
+        kinds = {
+          'm:modules',
+          'c:classes',
+          'd:describes',
+          'C:contexts',
+          'i:things described with "it"',
+          'f:methods',
+          'F:singleton methods',
+        }
+      }
+    end
+  }
+}
