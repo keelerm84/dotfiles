fc27537e25b2baaea9d79ac7871087cb1ffb2d17
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-06 23:22:27 -0400

    vim: Add treesitter plugin

---
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      local configuration = vim.fn['gruvbox_material#get_configuration']()
+      local palette = vim.fn['gruvbox_material#get_palette'](configuration.background, configuration.foreground, configuration.colors_override)
+
+      vim.api.nvim_set_hl(0, "@variable.php", { fg = palette.blue[1] })
+    end,
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true })
+    end,
+    opts = {
+      indent = {
+        enable = true,
+      },
+      ensure_installed = {
+        'bash',
+        'c',
+        'cpp',
+        'c_sharp',
+        'css',
+        'dockerfile',
+        'elixir',
+        'go',
+        'graphql',
+        'haskell',
+        'html',
+        'java',
+        'javascript',
+        'javascript',
+        'json',
+        'json',
+        'latex',
+        'lua',
+        'php',
+        'phpdoc',
+        'python',
+        'ruby',
+        'rust',
+        'swift',
+        'terraform',
+        'toml',
+        'typescript',
+        'twig',
+        'vim',
+        'yaml',
+        'zig',
+      }
+    }
+  }
+}
