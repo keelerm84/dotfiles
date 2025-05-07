8719fda7c0139dd78e71fec4fcb31f871313ec37
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-07 18:49:51 -0400

    vim: Fix fzf colors

--- home/.config/nvim/lua/plugins/fzf.lua
+++ home/.config/nvim/lua/plugins/fzf.lua
@@ -8,7 +8,10 @@ return {
 
   {
     'junegunn/fzf.vim',
-    dependencies = { 'junegunn/fzf' },
+    dependencies = {
+      'junegunn/fzf',
+      'sainnhe/gruvbox-material'
+    },
     keys = {
       { '<C-p>', ':call SmartFzfSearching()<CR>', silent = true },
       { 'g<C-p>', ':Files<CR>', silent = true },
@@ -40,18 +43,22 @@ return {
       }
 
       vim.g.fzf_preview_window = {'right:50%:hidden', 'ctrl-o'}
+    end,
+    config = function()
+      local configuration = vim.fn['gruvbox_material#get_configuration']()
+      local palette = vim.fn['gruvbox_material#get_palette'](configuration.background, configuration.foreground, configuration.colors_override)
 
       vim.g.fzf_colors = {
         ['fg'] = {'fg', 'Normal'},
         ['bg'] = {'bg', 'Normal'},
-        ['hl'] = {'fg', 'GruvboxAqua'},
+        ['hl'] = {'fg', palette.aqua[1]},
         ['fg+'] = {'fg', 'String'},
         ['bg+'] = {'bg', 'Normal'},
-        ['hl+'] = {'fg', 'GruvboxBlue'},
+        ['hl+'] = {'fg', palette.blue[1]},
         ['info'] = {'fg', 'PreProc'},
         ['border'] = {'fg', 'Comment'},
-        ['prompt'] = {'fg', 'GruvboxPurple'},
-        ['pointer'] = {'fg', 'GruvboxPurple'},
+        ['prompt'] = {'fg', palette.purple[1]},
+        ['pointer'] = {'fg', palette.purple[1]},
         ['marker'] = {'fg', 'Keyword'},
         ['spinner'] = {'fg', 'Label'},
         ['header'] = {'fg', 'Comment'}
