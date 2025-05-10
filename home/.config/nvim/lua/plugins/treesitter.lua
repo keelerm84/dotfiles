85878e78c137351ecafa19dab42a8c9dab9994a3
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-10 01:08:38 -0400

    vim: More tree sitter configurations

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -10,48 +10,67 @@ return {
       local palette = vim.fn['gruvbox_material#get_palette'](configuration.background, configuration.foreground, configuration.colors_override)
 
       vim.api.nvim_set_hl(0, "@variable.php", { fg = palette.blue[1] })
+
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+        ensure_installed = {
+          'bash',
+          'c',
+          'cpp',
+          'c_sharp',
+          'css',
+          'dart',
+          'diff',
+          'dockerfile',
+          'elixir',
+          'gitignore',
+          'go',
+          'gomod',
+          'gosum',
+          'gotmpl',
+          'graphql',
+          'haskell',
+          'hcl',
+          'helm',
+          'html',
+          'java',
+          'javascript',
+          'json',
+          'latex',
+          'lua',
+          'make',
+          'markdown',
+          'markdown_inline',
+          'php',
+          'phpdoc',
+          'python',
+          'regex',
+          'ruby',
+          'rust',
+          'scss',
+          'sql',
+          'swift',
+          'terraform',
+          'toml',
+          'tsx',
+          'twig',
+          'typescript',
+          'vim',
+          'vimdoc',
+          'yaml',
+          'zig',
+        }
+
+      })
     end,
     build = function()
       require('nvim-treesitter.install').update({ with_sync = true })
     end,
-    opts = {
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'c_sharp',
-        'css',
-        'dart',
-        'dockerfile',
-        'elixir',
-        'go',
-        'graphql',
-        'haskell',
-        'html',
-        'java',
-        'javascript',
-        'javascript',
-        'json',
-        'json',
-        'latex',
-        'lua',
-        'php',
-        'phpdoc',
-        'python',
-        'ruby',
-        'rust',
-        'swift',
-        'terraform',
-        'toml',
-        'typescript',
-        'twig',
-        'vim',
-        'yaml',
-        'zig',
-      }
-    }
   }
 }
