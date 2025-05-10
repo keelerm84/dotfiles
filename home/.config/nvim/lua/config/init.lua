0e2c57ae3f13427250431b24b27932f4f485e480
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-10 17:23:49 -0400

    vim: Move lazy to its own file

--- home/.config/nvim/lua/config/init.lua
+++ home/.config/nvim/lua/config/init.lua
@@ -1,34 +1,7 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("config.options")
 require("config.functions")
 require("config.keymaps")
 require("config.autocmd")
+require("config.lazy")
 
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-  change_detection = { enabled = false },
-})
+pcall(require, 'local.init')
