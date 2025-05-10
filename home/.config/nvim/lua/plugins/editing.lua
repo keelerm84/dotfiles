af5e0127e1aa119439b30c6abd432cc5367e50bd
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-10 17:24:03 -0400

    vim: Add autopairs support

--- home/.config/nvim/lua/plugins/editing.lua
+++ home/.config/nvim/lua/plugins/editing.lua
@@ -31,4 +31,29 @@ return {
       ft_blocklist = {"markdown"}
     },
   },
+
+  {
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      -- import nvim-autopairs
+      local autopairs = require("nvim-autopairs")
+
+      -- configure autopairs
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+          javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+          java = false, -- don't check treesitter on java
+        },
+      })
+
+      -- make autopairs and completion work together
+      require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+    end,
+  }
 }
