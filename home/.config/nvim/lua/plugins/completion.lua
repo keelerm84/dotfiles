8a67d1cdba7ebc15e0f02f416d496f867a7b563b
Matthew M. Keeler <keelerm84@gmail.com>
2025-05-15 20:21:29 -0400

    vim: Formatting for completion.lua

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -1,34 +1,33 @@
 return {
   {
-    'L3MON4D3/LuaSnip',
-    version = 'v2.*',
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
     config = function()
       require("luasnip.loaders.from_lua").load({
-        paths = vim.fn.stdpath('config') .. "/lua/snippets"
+        paths = vim.fn.stdpath("config") .. "/lua/snippets",
       })
-    end
+    end,
   },
 
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      'L3MON4D3/LuaSnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
-      'saadparwaiz1/cmp_luasnip',
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets", -- useful snippets
       "onsails/lspkind.nvim", -- vs-code like pictograms
     },
     config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
       local lspkind = require("lspkind")
 
-
       local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line, col = (table.unpack or unpack)(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
@@ -45,19 +44,18 @@ return {
 
         -- Key mapping
         mapping = {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<C-y>'] = cmp.mapping.confirm {
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<C-y>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = false,
-          },
+          }),
           -- Tab mapping
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            -- local copilot_keys = vim.fn['copilot#Accept']()
+          ["<Tab>"] = cmp.mapping(function(fallback)
             if luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             elseif cmp.visible() then
@@ -71,7 +69,7 @@ return {
             end
           end, { "i", "s" }),
 
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -84,11 +82,11 @@ return {
 
         -- Load sources, see: https://github.com/topics/nvim-cmp
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-          { name = 'buffer' },
-          { name = 'copilot' },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "buffer" },
+          { name = "copilot" },
         },
 
         formatting = {
@@ -97,7 +95,6 @@ return {
             ellipsis_char = "...",
           }),
         },
-
       })
 
       cmp.event:on("menu_opened", function()
@@ -107,6 +104,6 @@ return {
       cmp.event:on("menu_closed", function()
         vim.b.copilot_suggestion_hidden = false
       end)
-    end
-  }
+    end,
+  },
 }
