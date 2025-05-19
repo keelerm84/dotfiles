be354ccb1fbae298eda37617a324971423cb8dfa
Matthew Keeler <mkeeler@launchdarkly.com>
2025-05-19 14:20:40 -0400

    vim: Stop hiding copilot suggestion on cmp menu

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -96,14 +96,6 @@ return {
           }),
         },
       })
-
-      cmp.event:on("menu_opened", function()
-        vim.b.copilot_suggestion_hidden = true
-      end)
-
-      cmp.event:on("menu_closed", function()
-        vim.b.copilot_suggestion_hidden = false
-      end)
     end,
   },
 }
