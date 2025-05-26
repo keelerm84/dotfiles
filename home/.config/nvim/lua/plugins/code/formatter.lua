7af23af418755c4f0b2a560f22d21c7519abbfdd
Beshoy Girgis <shoy@1ds.us>
2025-05-26 10:50:48 -0500

    vim: Enable auto formatting toggle
    Signed-off-by: Beshoy Girgis <shoy@1ds.us>

--- home/.config/nvim/lua/plugins/code/formatter.lua
+++ home/.config/nvim/lua/plugins/code/formatter.lua
@@ -1,12 +1,37 @@
+vim.g.disable_autoformat = false
+vim.b.disable_autoformat = false
+
 return {
   {
     "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    keys = {
+      {
+        "<leader>fa",
+        function()
+          if vim.g.disable_autoformat then
+            vim.g.disable_autoformat = false
+            vim.b.disable_autoformat = false
+            vim.notify("Auto formatting enabled")
+          else
+            vim.g.disable_autoformat = true
+            vim.b.disable_autoformat = true
+            vim.notify("Auto formatting disabled")
+          end
+        end,
+        mode = "",
+        desc = "Toggle auto formatting on save",
+      },
+    },
     config = function()
       require("conform").setup({
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
         formatters = {
           stylua = {
             prepend_args = {
