8a51cb18c7d5d7a685e779e927b507406b3eb79a
Matthew Keeler <mkeeler@launchdarkly.com>
2025-05-29 16:47:39 -0400

    Revert "vim: Disable dressing.nvim"
    This reverts commit 991003208e469447c6b6dd2bb0b79197de3fbdd0.

--- home/.config/nvim/lua/plugins/code/ai.lua
+++ home/.config/nvim/lua/plugins/code/ai.lua
@@ -20,14 +20,7 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      {
-        "stevearc/dressing.nvim", -- for dressing provider
-        opts = {
-          input = {
-            enabled = false,
-          },
-        },
-      },
+      "stevearc/dressing.nvim", -- for dressing provider
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
