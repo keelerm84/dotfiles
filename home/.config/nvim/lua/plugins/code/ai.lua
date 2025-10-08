adbbd9c06f36841fb93776d13b05825e71b13097
Matthew Keeler <mkeeler@launchdarkly.com>
2025-10-08 09:46:34 -0400

    nvim: Use MeanderingProgrammer/render-markdown.nvim outside of avante

--- home/.config/nvim/lua/plugins/code/ai.lua
+++ home/.config/nvim/lua/plugins/code/ai.lua
@@ -24,6 +24,7 @@ return {
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "MeanderingProgrammer/render-markdown.nvim", -- for markdown rendering in preview window
       {
         "zbirenbaum/copilot.lua",
         event = "InsertEnter",
@@ -57,14 +58,6 @@ return {
             use_absolute_path = true,
           },
         },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
       },
     },
   },
--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -40,6 +40,15 @@ return {
   },
 
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+
+  {
     "nvim-lualine/lualine.nvim",
     dependencies = {
       "kyazdani42/nvim-web-devicons",
