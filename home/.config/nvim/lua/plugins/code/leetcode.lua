96c7c851325c171b4e8c8386db6d7fc856bc127b
Matthew M. Keeler <keelerm84@gmail.com>
2026-02-07 16:49:57 -0500

    vim: Remove build step from leetcode plugin

--- home/.config/nvim/lua/plugins/code/leetcode.lua
+++ home/.config/nvim/lua/plugins/code/leetcode.lua
@@ -1,7 +1,6 @@
 return {
   {
     "kawre/leetcode.nvim",
-    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
     dependencies = {
       -- include a picker of your choice, see picker section for more details
       "nvim-lua/plenary.nvim",
